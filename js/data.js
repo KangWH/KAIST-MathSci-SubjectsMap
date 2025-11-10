a89d4173dba89d0414b7bea50c4615ee534b1e42
Woohyun Kang <30944771+KangWH@users.noreply.github.com>
2025-11-10 23:13:53 +0900

    Update data.js
    영어 과목명 추가

--- js/data.js
+++ js/data.js
@@ -9,17 +9,17 @@ mainData.addSubject(mas10002);
 const mas10009 = new Subject('MAS.10009', '기초선택', '선형대수학개론', 'Introduction to Linear Algebra', '3:0:3');
 mainData.addSubject(mas10009);
 
-const mas10010 = new Subject('MAS.10010', '기초선택', '데이터과학을 위한 선형대수학', 'Data Science Linear Algebra?', '3:0:3');
+const mas10010 = new Subject('MAS.10010', '기초선택', '데이터과학을 위한 선형대수학', 'Linear Algebra for Data Science', '3:0:3');
 mainData.addSubject(mas10010);
 
-const mas20001 = new Subject('MAS.20001', '기초선택', '응용미분방정식', 'Application of Differential Equations', '3:0:3');
+const mas20001 = new Subject('MAS.20001', '기초선택', '응용미분방정식', 'Differential Equations and Applications', '3:0:3');
 mainData.addSubject(mas20001);
 
-const mas20002 = new Subject('MAS.20002', '기초선택', '응용해석학', 'AppAny?', '3:0:3');
+const mas20002 = new Subject('MAS.20002', '기초선택', '응용해석학', 'Applied Mathematical Analysis', '3:0:3');
 mas20002.addPrerequisite('MAS.20001');
 mainData.addSubject(mas20002);
 
-const mas20050 = new Subject('MAS.20050', '기초선택', '확률및통계', 'Prob&Stat?', '3:0:3');
+const mas20050 = new Subject('MAS.20050', '기초선택', '확률및통계', 'Probability and Statistics', '3:0:3');
 mas20050.addPrerequisite('MAS.10002');
 mainData.addSubject(mas20050);
 
@@ -52,27 +52,27 @@ mas30012.addPrerequisite('MAS.20012');
 mas30012.addPrerequisite('MAS.30011');
 mainData.addSubject(mas30012);
 
-const mas30021 = new Subject('MAS.30021', '전공선택', '미분기하학개론', 'Introduction to Differential Algebra', '3:2:4');
+const mas30021 = new Subject('MAS.30021', '전공선택', '미분기하학개론', 'Introduction to Differential Geometry', '3:2:4');
 mas30021.addPrerequisite('MAS.20041');
 mainData.addSubject(mas30021);
 
 const mas30031 = new Subject('MAS.30031', '전공선택', '위상수학', 'Topology', '3:2:4');
 mainData.addSubject(mas30031);
 
-const mas30041 = new Subject('MAS.30041', '전공선택', '복소변수함수론', 'Complex?', '3:0:3');
+const mas30041 = new Subject('MAS.30041', '전공선택', '복소변수함수론', 'Complex Variables I', '3:0:3');
 mas30041.addPrerequisite('MAS.20041');
 mainData.addSubject(mas30041);
 
-const mas30050 = new Subject('MAS.30050', '전공선택', '기초확률론', 'Stat?', '3:0:3');
+const mas30050 = new Subject('MAS.30050', '전공선택', '기초확률론', 'Elementary Probability Theory', '3:0:3');
 mas30050.addPrerequisite('MAS.20050');
 mainData.addSubject(mas30050);
 
-const mas30055 = new Subject('MAS.30055', '전공선택', '수리통계학', 'Prob?', '3:0:3');
+const mas30055 = new Subject('MAS.30055', '전공선택', '수리통계학', 'Mathematical Statistics', '3:0:3');
 mas30055.addPrerequisite('MAS.20050');
 mas30055.addPrerequisite('MAS.20041');
 mainData.addSubject(mas30055);
 
-const mas30064 = new Subject('MAS.30064', '전공선택', '행렬계산과 응용', 'Mat?', '3:0:3');
+const mas30064 = new Subject('MAS.30064', '전공선택', '행렬계산과 응용', 'Matrix Computation and Application', '3:0:3');
 mas30064.addPrerequisite('MAS.10009');
 mainData.addSubject(mas30064);
 
@@ -80,7 +80,7 @@ const mas30065 = new Subject('MAS.30065', '전공선택', '수치해석학개론
 mas30065.addPrerequisite('MAS.10009');
 mainData.addSubject(mas30065);
 
-const mas30074 = new Subject('MAS.30074', '전공선택', '최적화이론', 'Optimization?', '3:0:3');
+const mas30074 = new Subject('MAS.30074', '전공선택', '최적화이론', 'Optimization Theory', '3:0:3');
 mas30074.addPrerequisite('MAS.10001');
 mas30074.addPrerequisite('MAS.10009');
 mainData.addSubject(mas30074);
@@ -104,7 +104,7 @@ mas40030.addPrerequisite('MAS.30011');
 mas40030.addPrerequisite('MAS.30031');
 mainData.addSubject(mas40030);
 
-const mas40035 = new Subject('MAS.40035', '전공선택', '행렬군론', 'Matrix group?', '3:0:3');
+const mas40035 = new Subject('MAS.40035', '전공선택', '행렬군론', 'Matrix Groups', '3:0:3');
 mainData.addSubject(mas40035);
 
 const mas40040 = new Subject('MAS.40035', '전공선택', '편미분방정식개론', 'Introduction to Partial Differential Equations', '3:0:3');
@@ -115,38 +115,38 @@ const mas40041 = new Subject('MAS.40041', '전공선택', '르베그적분론', 
 mas40041.addPrerequisite('MAS.20041');
 mainData.addSubject(mas40041);
 
-const mas40042 = new Subject('MAS.40042', '전공선택', '푸리에 해석과 응용', '', '3:0:3');
+const mas40042 = new Subject('MAS.40042', '전공선택', '푸리에 해석과 응용', 'Fourier Analysis and Applications', '3:0:3');
 mas40042.addPrerequisite('MAS.20042');
 mainData.addSubject(mas40042);
 
-const mas40043 = new Subject('MAS.40043', '전공선택', '상미분방정식과 동역학계', '', '3:0:3');
+const mas40043 = new Subject('MAS.40043', '전공선택', '상미분방정식과 동역학계', 'Ordinary Differential Equations and Dynamical Systems', '3:0:3');
 mas40043.addPrerequisite('MAS.20001');
 mas40043.addPrerequisite('MAS.20041');
 mainData.addSubject(mas40043);
 
-const mas40055 = new Subject('MAS.40055', '전공선택', '선형모형', '', '3:0:3');
+const mas40055 = new Subject('MAS.40055', '전공선택', '선형모형', 'Linear Model', '3:0:3');
 mas40055.addPrerequisite('MAS.10009');
 mainData.addSubject(mas40055);
 
-const mas40056 = new Subject('MAS.40056', '전공선택', '컴퓨터 통계방법론', '', '3:0:3');
+const mas40056 = new Subject('MAS.40056', '전공선택', '컴퓨터 통계방법론', 'Statistical Methods with Computer', '3:0:3');
 mas40056.addPrerequisite('MAS.20050');
 mainData.addSubject(mas40056);
 
-const mas40067 = new Subject('MAS.40067', '전공선택', '수리생물학개론', '', '3:0:3');
+const mas40067 = new Subject('MAS.40067', '전공선택', '수리생물학개론', 'Introduction to Mathematical Biology', '3:0:3');
 mas40067.addPrerequisite('MAS.10009');
 mas40067.addPrerequisite('MAS.20001');
 mas40067.addPrerequisite('MAS.20050');
 mainData.addSubject(mas40067);
 
-const mas40073 = new Subject('MAS.40073', '전공선택', '수학과 인공지능개론', '', '3:0:3');
+const mas40073 = new Subject('MAS.40073', '전공선택', '수학과 인공지능개론', 'Introduction to Artificial Intelligence with Mathematics', '3:0:3');
 mas40073.addPrerequisite('MAS.10009');
 mas40073.addPrerequisite('MAS.20050');
 mas40073.addPrerequisite('MAS.20041');
 mainData.addSubject(mas40073);
 
-const mas40077 = new Subject('MAS.40077', '전공선택', '그래프이론개론', '', '3:0:3');
+const mas40077 = new Subject('MAS.40077', '전공선택', '그래프이론개론', 'Introduction to Graph Theory', '3:0:3');
 mas40077.addPrerequisite('MAS.20075');
 mainData.addSubject(mas40077);
 
-const mas40078 = new Subject('MAS.40078', '전공선택', '이산기하', '', '3:0:3');
+const mas40078 = new Subject('MAS.40078', '전공선택', '이산기하', 'Discrete Geometry', '3:0:3');
 mainData.addSubject(mas40078);
